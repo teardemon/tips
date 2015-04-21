a0151efc7dfad47e85ac73d5a8959e3e5765e6fb
qiangyao <qy610426@yy.com>
2015-04-21 14:19:50 +0800

    产生10位长度的密码

--- py/gen_passwd.py
+++ py/gen_passwd.py
@@ -5,6 +5,7 @@ import random
 import string
 
 def GenPassWord(length=10,chars=string.ascii_letters+string.digits):
+    '''产生10位长度的密码'''
     return ''.join([random.choice(chars) for i in range(length)])
 
 
